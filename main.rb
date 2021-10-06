69634a0bf57c72827991421145fd9d3cc64f1894
Ryan Kubow <ryankubow@gmail.com>
2021-10-06 13:14:35 -0700

    changed variable names

--- main.rb
+++ main.rb
@@ -1,27 +1,24 @@
 require 'csv'
-# --FEATURE-- only process line one this way, but read all lines. Convert all info into hashes?
-=begin
-NOTES: First line of information is used for data specific to private user info/info about user.
-Second line and beyond is public tags of interests/locations and then description
-=end
 
-dataArr = CSV.read('C:\Users\Amazingg\Desktop\user_data.csv', liberal_parsing: true)
-userFields = dataArr.first.join(' ').split('~')
-userData = dataArr.drop(1).map {|arr| arr.join(' ').split('~') }
+csvArray = CSV.read('C:\Users\Amazingg\Desktop\user_data.csv', liberal_parsing: true)
+userFields = csvArray.first.join(' ').split('~')
+userData = csvArray.drop(1).map {|arr| arr.join(' ').split('~') }
 allUsers = Array.new(userData.length)
 allUsers.map! do |x|
     x = userFields.to_h { |field| [field, 'empty'] }
 end
 
-userNum = 0
+user = 0
 field = 0
-while userNum < allUsers.length do
+while user < allUsers.length do
     while field < userFields.length do
-        allUsers[userNum]["#{userFields[field]}"] = "#{userData[userNum][field]}"
+        key = "#{userFields[field]}"
+        value = "#{userData[user][field]}"
+        allUsers[user][key] = value
         field += 1
     end
     field = 0
-    userNum += 1
+    user += 1
 end
 
 for user in 0...allUsers.length do
